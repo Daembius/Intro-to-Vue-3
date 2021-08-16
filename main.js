d301946379eb25c53369006177117744c57ca608
Daembius <julienmayard@me.com>
2021-08-16 20:40:50 +0900

    1st lesson with exercice

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const  app = Vue.createApp({
+    data() {
+        return {
+            product : 'Chaussettes',
+            description : "Chaussettes originales en fil d’Écosse, réputées pour leur douceur et pour leur confort."
+        }
+    }
+})
